211b1d897b898ca6fefdb0ad9714ab289cc4e937
27236-tanawut <27236@bbw.ac.th>
2026-07-09 14:25:05 +0700

    Add files via upload

---
+++ max.py
@@ -0,0 +1,12 @@
+print("x", end="\t")
+for col in range(1, 13):
+    print(col, end="\t")
+print()
+ 
+for row in range(1, 13):
+    print(row, end="\t")
+    # แก้ไขการย่อหน้า (Indentation) ในบรรทัดด้านล่างนี้ให้ตรงกัน
+    for col in range(1, 13):
+        result = row * col
+        print(result, end="\t")
+    print()
